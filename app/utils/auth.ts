105d5a777a6ca4d4c3fea2b61bc21ae51668f4e0
groloff <groloff@gmail.com>
2023-12-30 11:06:18 -0500

    authoptions fixed

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,12 +1,18 @@
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
+import type { Adapter } from "next-auth/adapters"
+
+
+function getPrismaAdapter(): Adapter {
+  return PrismaAdapter(prisma) as Adapter
+}
 
 export const authOptions = {
-    adapter: PrismaAdapter(prisma),
+    adapter: getPrismaAdapter(),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
